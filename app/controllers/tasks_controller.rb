da8becea0c3bd5594a1ee09ca9f5096c585aabdf
Victor Sanchez <vsanantone@gmail.com>
2023-10-06 15:36:36 -0500

    Final Commit Rails Tutorial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,45 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["Task1", "Task2", "Task3"]
+    @tasks = Task.all
   end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    task = Task.new({
+      title: params[:title],
+      description: params[:description]
+    })
+    # binding.pry
+    task.save
+    # binding.pry
+    redirect_to '/tasks'
+  end
+
+  def show
+    # binding.pry
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:title],
+      description: params[:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   get "/", to: "welcome#index"
 
   get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id", to: "tasks#show"
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
